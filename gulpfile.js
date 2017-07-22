e6e529bfc84abb8cebb036c838d231412dd81a00
xander <besolov@odva.pro>
2017-07-22 16:25:01 +0300

    ramarks in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,26 +16,26 @@ var gulp		  = require('gulp'),
 
 // таск для компиляции scss в css
 gulp.task('sass', function() {
-	return gulp.src('html/scss/style.scss')
+	return gulp.src('scss/style.scss')
 	.pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-	.pipe(gulp.dest('html/css'))
+	.pipe(gulp.dest('css'))
 	.pipe(browserSync.reload({stream: true}))
 });
 
 // таск для сжатия и объединения js файлов
 gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
-		'html/js/libs/jquery-2.2.2.min.js',
-		'html/js/libs/jquery.formstyler.min.js',
-		'html/js/libs/input-mask.min.js',
-		'html/js/libs/slick.min.js',
-		'html/js/libs/object-fit-polyfill.js',
-		'html/js/partials/script.js',
+		'js/vendors/jquery-2.2.2.min.js',
+		'js/vendors/jquery.formstyler.min.js',
+		'js/vendors/input-mask.min.js',
+		'js/vendors/slick.min.js',
+		'js/vendors/object-fit-polyfill.js',
+		'js/main.js',
 		])
-		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+		.pipe(concat('main.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		//.pipe(uglify()) // Сжимаем JS файл
-		.pipe(gulp.dest('html/js')); // Выгружаем в папку app/js
+		.pipe(gulp.dest('js')); // Выгружаем в папку app/js
 });
 
 // конфиг для создания спрайтов
@@ -45,11 +45,11 @@ var svgSpriteConfig = {
 	mode : {
 		 css : {
 			 dest : '.',
-			 sprite : 'html/images/sprite.svg',
+			 sprite : 'images/sprite.svg',
 			 render : {
-				 css : {dest : 'html/css/sprite.css'},
+				 css : {dest : 'css/sprite.css'},
 				 scss : {
-					 dest : 'html/scss/sprite.scss'
+					 dest : 'scss/sprite.scss'
 				 }
 			 },
 		 }
@@ -58,20 +58,20 @@ var svgSpriteConfig = {
 
 // таск для создания спрайтов из png картинок
 gulp.task('sprite', function () {
-    var spriteData = gulp.src('html/images/icons/*.png').pipe(spritesmith({
+    var spriteData = gulp.src('images/icons/*.png').pipe(spritesmith({
         imgName: 'sprite.png',
         imgPath: '/images/sprite.png',
         cssName: 'sprite.scss',
         algorithm: 'diagonal',
         paddingg: 15
     }));
-    spriteData.img.pipe(gulp.dest('html/images/'));
-    spriteData.css.pipe(gulp.dest('html/scss/components/'));
+    spriteData.img.pipe(gulp.dest('images/'));
+    spriteData.css.pipe(gulp.dest('scss/components/'));
 });
 
 // таск для создания спрайтов из svg картинок
 gulp.task('svg:build', function () {
-	return gulp.src('html/images/svg/*.svg')
+	return gulp.src('images/svg/*.svg')
 		.pipe(svgSprite(svgSpriteConfig))
 		.pipe(gulp.dest("."));
 });
@@ -79,19 +79,19 @@ gulp.task('svg:build', function () {
 // fileinclude - простейший шабланизатор
 gulp.task('fileinclude', function()
 {
-	gulp.src('html/src/*.html') // откуда брать файлы
+	gulp.src('src/*.html') // откуда брать файлы
 	.pipe(fileinclude({
 		prefix: '@@',
 		basepath: '@file'
 	}))
-	.pipe(gulp.dest("./html")); // сюда кладется скомпилированные html файлы
+	.pipe(gulp.dest("./")); // сюда кладется скомпилированные html файлы
 });
 
 // таск для обновления страницы
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
-			baseDir: 'html'
+			baseDir: './'
 		},
 		notify: false
 	})
@@ -99,17 +99,17 @@ gulp.task('browser-sync', function() {
 
 // таск следит за изменениями файлов и вызывает другие таски
 gulp.task('watch', function() {
-	gulp.watch('html/scss/**/*.scss',['sass']);
-	gulp.watch(['html/src/*.html','html/src/inc/**/*.html'],['fileinclude']);
-	gulp.watch(['html/js/libs/*.js', 'html/js/partials/*.js'],['scripts']);
-	gulp.watch('html/inc/*.html', browserSync.reload);
-	gulp.watch('html/**/*.html', browserSync.reload);
-	gulp.watch('html/js/*.js', browserSync.reload);
+	gulp.watch('scss/**/*.scss',['sass']);
+	gulp.watch(['src/*.html','src/inc/**/*.html'],['fileinclude']);
+	gulp.watch(['js/vendors/*.js', 'js/main.js', 'js/modules/*.js'],['scripts']);
+	gulp.watch('inc/*.html', browserSync.reload);
+	gulp.watch('**/*.html', browserSync.reload);
+	gulp.watch('js/*.js', browserSync.reload);
 });
 
 // таск сжимает картинки без потери качества 
 gulp.task('img', function() {
-	return gulp.src('html/images/**/*') // откуда брать картинки
+	return gulp.src('images/**/*') // откуда брать картинки
 	.pipe(cache(imagemin({
 		interlaced: true,
 		progressive: true,
