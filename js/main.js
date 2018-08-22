60e5e8ad67a9bc677deaded81ef3739700024e12
Alan Sokurov <sokalcc@gmail.com>
2018-08-22 16:01:03 +0300

    popup script bug fix

--- js/main.js
+++ js/main.js
@@ -26,12 +26,12 @@ var o2 =
 		showPopup: function(popup)
 		{
 			$('._overlay').addClass('_show');
-			$('._standartPopup').addClass('_show');
+			$(popup).addClass('_show');
 		},
 		closePopup: function(popup)
 		{
 			$('._overlay').removeClass('_show');
-			$('.popup').removeClass('_show');
+			$(popup).removeClass('_show');
 		},
 	},
 	makePhoneMask: function()
