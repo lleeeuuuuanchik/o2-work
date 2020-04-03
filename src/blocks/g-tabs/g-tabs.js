88d20d6a214823fc6f281041fc9c9131278c5bea
Tylex <tylex@MacBook-Pro-Tylex.local>
2020-04-03 16:28:49 +0300

    ui components

--- src/blocks/g-tabs/g-tabs.js
+++ src/blocks/g-tabs/g-tabs.js
@@ -4,20 +4,22 @@ o2.gTabs = {
 	{
 		const tabsContainer = $(instance).parents('._tabs-container');
 		const openedTab = $(tabsContainer).find('.g-tab[data-tab-id="' + tabId + '"]');
+		const tab = $(tabsContainer).find('.g-tab');
 
 		if ($(openedTab).hasClass('g-tab_open'))
 			return false;
 
 		$(tabsContainer).find('.g-tab.g-tab_open').fadeOut(200, () =>
 		{
-			$('.g-tab').removeClass('g-tab_open')
+			$(tab).removeClass('g-tab_open')
 			$(openedTab).fadeIn(200).addClass('g-tab_open');
 		});
 		this.markActiveTab(instance)
 	},
 	markActiveTab(activeItem)
 	{
-		$('.g-tabs__item').each((index, item) =>
+		const tabItems = $(activeItem).parents('._tabs-container').find('.g-tabs__item');
+		$(tabItems).each((index, item) =>
 		{
 			if ($(item).hasClass('g-tabs__item_active'))
 				$(item).removeClass('g-tabs__item_active');
