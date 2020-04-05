2bc5a48dd99bcda039c49683cd16ab291d4f6fb0
Михаил Дзантиев <axel0726@gmail.com>
2020-04-05 17:06:41 +0300

    range slider

--- src/blocks/g-range-slider/g-range-slider.js
+++ src/blocks/g-range-slider/g-range-slider.js
@@ -2,48 +2,64 @@ o2.gRangeSlider =
 {
 	init()
 	{
-		$('._range-trigger').each((index, element) => {
+		$('._range-slider').each((index, element) => {
 			const dataParams = $(element).data();
-			const minName = $(element).attr('data-field-min-name');
-			const maxName = $(element).attr('data-field-max-name');
-			const type = $(element).attr('data-type');
+			let start = $(element).data('start');
+			let connect = [true,false];
+			if(typeof start == 'string')
+			{
+				start = start.split(',');
+				connect = true;
+			}
 
-			const slider = noUiSlider.create(element, {
-				start: [dataParams.from, dataParams.to],
-				connect: true,
-				tooltips: true,
-				step: 1,
-				range:
-				{
-					'min': dataParams.min,
-					'max': dataParams.max
-				}
+			const slider = noUiSlider.create(element,{
+				start    : start,
+				connect  : connect,
+				tooltips : true,
+				format   : this.format,
+				step     : 1,
+				range    : {'min': dataParams.min, 'max': dataParams.max }
 			});
-			if (type == 'single')
-				$(element).find('.noUi-origin').first().attr('disabled', true);
-			const moneyFormat = (number) => {
-				return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-			};
-			// настраиваем лэйблы для слайдера
-			const sliderLabels = $(element).siblings('.g-range-slider-labels');
-			if (sliderLabels.length) {
-				const labelFrom = sliderLabels.find('.g-range-slider-labels_from');
-				const labelTo = sliderLabels.find('.g-range-slider-labels_to');
-				slider.on('update', function (values) {
-					labelFrom.html(moneyFormat(Number(values[0])));
-					labelTo.html(moneyFormat(Number(values[1])));
-				});
-			};
-			slider.on('set', function (values, handle) {
-				if (handle == 0)
-					$(this.target).siblings('input[name="' + minName + '"]').trigger('change')
-				else
-					$(this.target).siblings('input[name="' + maxName + '"]').trigger('change')
-			});
-			slider.on('update', function (values) {
-				$(this.target).siblings('input[name="' + minName + '"]').val(values[0]);
-				$(this.target).siblings('input[name="' + maxName + '"]').val(values[1]);
-			});
+
+			slider.on('set', this.set);
+			slider.on('update', this.update);
 		})
-	}
+	},
+	set(values, handle)
+	{
+		if (handle == 0)
+			$(this.target).siblings('._min').trigger('change');
+		else
+			$(this.target).siblings('._max').trigger('change');
+	},
+	update(values)
+	{
+		let $slider = $(this.target).parents('._range-slider-wrap');
+		let firstValue = o2.gRangeSlider.format.from(values[0].toString());
+		$slider.find('._min').val(firstValue);
+		$slider.find('._label-to').html(values[0]);
+
+		let from = o2.gRangeSlider.format.to(this.options.range.min);
+		$slider.find('._label-from').html(from);
+
+		if(values.length > 1)
+		{
+			let secondVal = o2.gRangeSlider.format.from(values[1].toString());
+			$slider.find('._max').val(secondVal);
+			$slider.find('._label-from').html(values[0]);
+			$slider.find('._label-to').html(values[1]);
+		}
+	},
+	format:
+	{
+        to(number)
+        {
+        	number = Math.round(number);
+        	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        },
+        from(stringNumber)
+        {
+        	return Number(stringNumber.replace(/[ ,\-]+/, ''));
+        }
+    }
 };
