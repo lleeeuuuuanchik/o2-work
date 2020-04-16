a4bdeeff506e07c6f9e7b158f508c6975e118349
Михаил Дзантиев <axel0726@gmail.com>
2020-04-17 00:36:14 +0300

    tabs

--- src/blocks/g-tabs/g-tabs.js
+++ src/blocks/g-tabs/g-tabs.js
@@ -1,30 +1,16 @@
 "use strict"
-o2.gTabs = {
+o2.gTabs =
+{
 	open(instance, tabId)
 	{
-		const tabsContainer = $(instance).parents('._tabs-container');
-		const openedTab = $(tabsContainer).find('.g-tab[data-tab-id="' + tabId + '"]');
-		const tab = $(tabsContainer).find('.g-tab');
-
-		if ($(openedTab).hasClass('g-tab_open'))
-			return false;
-
-		$(tabsContainer).find('.g-tab.g-tab_open').fadeOut(200, () =>
-		{
-			$(tab).removeClass('g-tab_open')
-			$(openedTab).fadeIn(200).addClass('g-tab_open');
-		});
-		this.markActiveTab(instance)
+		this.activateTab(instance);
+		const $tabsContainer = $(instance).parents('._tabs-container');
+		$tabsContainer.find("[data-tab]").removeClass('open');
+		$tabsContainer.find(`[data-tab="${tabId}"]`).addClass('open');
 	},
-	markActiveTab(activeItem)
+	activateTab(activeTab)
 	{
-		const tabItems = $(activeItem).parents('._tabs-container').find('.g-tabs__item');
-		$(tabItems).each((index, item) =>
-		{
-			if ($(item).hasClass('g-tabs__item_active'))
-				$(item).removeClass('g-tabs__item_active');
-
-			$(activeItem).addClass('g-tabs__item_active');
-		})
+		$(activeTab).siblings('._tab').removeClass('active');
+		$(activeTab).addClass('active');
 	}
 };
